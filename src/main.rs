df9f6509944ef8078b97e391fd253da2afd83772
javierpb275 <javierpb275@gmail.com>
2022-03-02 18:02:00 +0100

    control flow: if, else and else if

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,23 @@
 fn main() {
 
-    //Functions
+    //control flow
 
-    println!("hello world!");
+    //if, else, else if
+    let number = 6;
 
-    //parameter
+    if number % 4 == 0 {
+        println!("number is divisible by 4");
+    } else if number % 3 == 0 {
+        println!("number is divisible by 3");
+    } else if number % 2 == 0 {
+        println!("number is divisible by 2");
+    } else {
+        println!("number is not divisible by 4, 3, or 2");
+    }
 
-    another_function(3);
+    let condition = true;
+    let number = if condition { 5 } else { 6 };
 
-    print_labeled_measurement(5, 'h');
-
-    //return
-
-    let x = five();
-
-    println!("The value of x is: {}", x);
-
-    let y = plus_one(5);
-
-    println!("The value of y is: {}", y);
+    println!("The value of number is: {}", number);
 
 }
-
-//functions with parameters
-
-fn another_function(x: i32) {
-    println!("af: the value is {}", x);
-}
-
-fn print_labeled_measurement(value: i32, unit_label: char) {
-    println!("The measurement is: {}{}", value, unit_label);
-}
-
-//functions with return values
-fn five() -> i32 {
-    5
-}
-
-fn plus_one(x: i32) -> i32 {
-    x + 1
-}
-
-//This function would give an error:
-fn plus_one_error(x: i32) -> i32 {
-    x + 1;
-}
-
-//if we add the ";" at the end we say that it is a statement and therefore we do NOT return the value.
-//if we do NOT add the ";" it is an expression and we RETURN the value of that expression.
-//you could use the keyword "return".
