08a53a66bcd0cff09b8396855476b44501910221
javierpb275 <javierpb275@gmail.com>
2022-03-09 20:49:22 +0100

    Ownership: Ways Variables and Data Interact: Move

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,29 @@ fn main() {
 
     //ownership
 
-    //memory and allocation
+    //Ways Variables and Data Interact: Move
 
-    {
-        let s = String::from("hello"); // s is valid from this point forward
+    let s1 = String::from("hello");
+    let s2 = s1;
 
-        // do stuff with s
-    }                                  // this scope is now over, and s is no
-                                       // longer valid
+    println!("{}, world!", s1);
 }
 
 /*
-Rust takes a different path: the memory is automatically returned once the variable that owns it goes out of scope.
-There is a natural point at which we can return the memory our String needs to the allocator: 
-when s goes out of scope. When a variable goes out of scope, Rust calls a special function for us. 
-This function is called drop, and it’s where the author of String can put the code to return the memory. 
-Rust calls drop automatically at the closing curly bracket.
+To ensure memory safety, after the line let s2 = s1, Rust considers s1 as no longer valid. 
+Therefore, Rust doesn’t need to free anything when s1 goes out of scope.
+You’ll get an error like this because Rust prevents you from using the invalidated reference
+
+If you’ve heard the terms shallow copy and deep copy while working with other languages, 
+the concept of copying the pointer, length, and capacity without copying the data probably 
+sounds like making a shallow copy. But because Rust also invalidates the first variable, 
+instead of calling it a shallow copy, it’s known as a move. In this example, we would 
+say that s1 was moved into s2.
+
+That solves our problem! With only s2 valid, when it goes out of scope, it alone will free the memory, 
+and we’re done.
+
+In addition, there’s a design choice that’s implied by this: Rust will never automatically 
+create “deep” copies of your data. Therefore, any automatic copying can be assumed to be inexpensive 
+in terms of runtime performance.
 */
