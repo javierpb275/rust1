2698fee789d2a9a063e6611b6f4de18e4ba3830d
javierpb275 <javierpb275@gmail.com>
2022-03-09 20:58:55 +0100

    ownership: return values and scope

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,36 @@
 fn main() {
 
-    //ownership and functions 
+    //Return Values and Scope
     
-    let s = String::from("hello");  // s comes into scope
+    let s1 = gives_ownership();         // gives_ownership moves its return
+                                        // value into s1
 
-    takes_ownership(s);             // s's value moves into the function...
-                                    // ... and so is no longer valid here
+    let s2 = String::from("hello");     // s2 comes into scope
 
-    let x = 5;                      // x comes into scope
+    let s3 = takes_and_gives_back(s2);  // s2 is moved into
+                                        // takes_and_gives_back, which also
+                                        // moves its return value into s3
+} // Here, s3 goes out of scope and is dropped. s2 was moved, so nothing
+  // happens. s1 goes out of scope and is dropped.
 
-    makes_copy(x);                  // x would move into the function,
-                                    // but i32 is Copy, so it's okay to still
-                                    // use x afterward
+fn gives_ownership() -> String {             // gives_ownership will move its
+                                             // return value into the function
+                                             // that calls it
 
-} // Here, x goes out of scope, then s. But because s's value was moved, nothing
-  // special happens.
+    let some_string = String::from("yours"); // some_string comes into scope
 
-fn takes_ownership(some_string: String) { // some_string comes into scope
-    println!("{}", some_string);
-} // Here, some_string goes out of scope and `drop` is called. The backing
-  // memory is freed.
+    some_string                              // some_string is returned and
+                                             // moves out to the calling
+                                             // function
+}
 
-fn makes_copy(some_integer: i32) { // some_integer comes into scope
-    println!("{}", some_integer);
-} // Here, some_integer goes out of scope. Nothing special happens.
+// This function takes a String and returns one
+fn takes_and_gives_back(a_string: String) -> String { // a_string comes into
+                                                      // scope
+
+    a_string  // a_string is returned and moves out to the calling function
+}
+
 
 /*
 
