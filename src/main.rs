d5ff5b19574953131a8837046e9341f1cdb77f07
javierpb275 <javierpb275@gmail.com>
2022-03-09 20:56:17 +0100

    Ownership and Functions

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,30 @@
 fn main() {
 
-    //ownership
+    //ownership and functions 
+    
+    let s = String::from("hello");  // s comes into scope
 
-    //Stack-Only Data: Copy
+    takes_ownership(s);             // s's value moves into the function...
+                                    // ... and so is no longer valid here
 
-    let x = 5;
-    let y = x;
+    let x = 5;                      // x comes into scope
 
-    println!("x = {}, y = {}", x, y);
-}
+    makes_copy(x);                  // x would move into the function,
+                                    // but i32 is Copy, so it's okay to still
+                                    // use x afterward
+
+} // Here, x goes out of scope, then s. But because s's value was moved, nothing
+  // special happens.
+
+fn takes_ownership(some_string: String) { // some_string comes into scope
+    println!("{}", some_string);
+} // Here, some_string goes out of scope and `drop` is called. The backing
+  // memory is freed.
+
+fn makes_copy(some_integer: i32) { // some_integer comes into scope
+    println!("{}", some_integer);
+} // Here, some_integer goes out of scope. Nothing special happens.
 
 /*
-types such as integers that have a known size at compile time are stored entirely on the stack, 
-so copies of the actual values are quick to make. That means there’s no reason we would want to 
-prevent x from being valid after we create the variable y. In other words, there’s no difference 
-between deep and shallow copying here, so calling clone wouldn’t do anything different from the 
-usual shallow copying and we can leave it out.
 
-Rust has a special annotation called the Copy trait that we can place on types that are stored 
-on the stack like integers are. If a type implements the Copy trait, a variable is still valid 
-after assignment to another variable.
 */
