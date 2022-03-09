7d0797cc3ebacd82900fc50a6237571b7772f790
javierpb275 <javierpb275@gmail.com>
2022-03-09 20:35:15 +0100

    ownership: string type

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,19 @@
 fn main() {
 
-    //control flow
+    //ownership
 
-    for number in (1..4).rev() {
-        println!("{}!", number);
-    }
-    println!("LIFTOFF!!!");
-    //3! 2! 1! LIFTOFF!!!
+    //string type
+
+    let mut s = String::from("hello");
+
+    s.push_str(", world!"); // push_str() appends a literal to a String
+
+    println!("{}", s); // This will print `hello, world!`
+
+    /*
+    The double colon :: operator allows us to namespace this particular from function 
+    under the String type rather than using some sort of name like string_from. 
+    when we call String::from, its implementation requests the memory it needs. 
+    This is pretty much universal in programming languages.
+    */
 }
