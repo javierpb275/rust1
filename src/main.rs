14b5d8a73165dd0f7de8d24fa9f3e975db299a20
javierpb275 <javierpb275@gmail.com>
2022-03-09 20:54:18 +0100

    ownership: Stack-Only Data: Copy

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,22 @@ fn main() {
 
     //ownership
 
-    //Ways Variables and Data Interact: Clone
+    //Stack-Only Data: Copy
 
-    let s1 = String::from("hello");
-    let s2 = s1.clone();
+    let x = 5;
+    let y = x;
 
-    println!("s1 = {}, s2 = {}", s1, s2);
+    println!("x = {}, y = {}", x, y);
 }
 
 /*
+types such as integers that have a known size at compile time are stored entirely on the stack, 
+so copies of the actual values are quick to make. That means there’s no reason we would want to 
+prevent x from being valid after we create the variable y. In other words, there’s no difference 
+between deep and shallow copying here, so calling clone wouldn’t do anything different from the 
+usual shallow copying and we can leave it out.
 
+Rust has a special annotation called the Copy trait that we can place on types that are stored 
+on the stack like integers are. If a type implements the Copy trait, a variable is still valid 
+after assignment to another variable.
 */
