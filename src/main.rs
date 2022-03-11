450d0efdeda750943470beb16ccd363e473ad8c0
javierpb275 <javierpb275@gmail.com>
2022-03-11 20:06:21 +0100

    references

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,24 @@
 fn main() {
+
+    //References
+
     let s1 = String::from("hello");
 
-    let (s2, len) = calculate_length(s1);
+    let len = calculate_length(&s1);
 
-    println!("The length of '{}' is {}.", s2, len);
+    println!("The length of '{}' is {}.", s1, len);
 }
 
-fn calculate_length(s: String) -> (String, usize) {
-    let length = s.len(); // len() returns the length of a String
-
-    (s, length)
-}
+fn calculate_length(s: &String) -> usize { // s is a reference to a String
+    s.len()
+} // Here, s goes out of scope. But because it does not have ownership of what
+  // it refers to, nothing happens.
 
 /*
-Rust does let us return multiple values using a tuple
+A reference is like a pointer in that it’s an address we can follow to access data 
+stored at that address that is owned by some other variable. Unlike a pointer, 
+a reference is guaranteed to point to a valid value of a particular type. 
+
+These ampersands represent references, and they allow you to refer to 
+some value without taking ownership of it.
 */
